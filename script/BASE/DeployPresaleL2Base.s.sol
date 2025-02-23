e1caa252700e07c821ae8d4f9f8e3e32931cbf47
CryptoGhost277 <cryptoghost277@gmail.com>
2025-02-23 12:22:27 +0100

    update deploy scripts BSC-BASE

--- script/BASE/DeployPresaleL2Base.s.sol
+++ script/BASE/DeployPresaleL2Base.s.sol
@@ -3,27 +3,27 @@
 pragma solidity 0.8.24;
 
 import {Script} from "forge-std/Script.sol";
-import {PresaleL2} from "../src/PresaleL2.sol";
+import {PresaleL2} from "../../src/PresaleL2.sol";
 import "forge-std/Test.sol";
 
-contract DeployPresaleL2 is Script {
+contract DeployPresaleL2Base is Script {
     uint256[][3] phases_;
 
     function run() external returns (PresaleL2) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        address usdtAddress_ = 0x55d398326f99059fF775485246999027B3197955; // @audit CAMBIAR A FINAL
-        address usdcAddress_ = 0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d; // @audit CAMBIAR A FINAL
-        address aggregatorContract_ = 0x9ef1B8c0E4F7dc8bF5719Ea496883DC6401d5b2e; // @audit CAMBIAR A FINAL
-        address paymentWallet_ = vm.addr(2); // @audit CAMBIAR A FINAL
+        address usdtAddress_ = 0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2; // USDT BASE
+        address usdcAddress_ = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913; // USDC BASE
+        address aggregatorContract_ = 0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70; // PriceFeed ETH/USD en BASE
+        address paymentWallet_ = 0x56E4CF839281f06c6B25a2037C5797C40D35fF2c; 
         uint256 maxTotalSellingAmount_ = 10000000000000 * 1e18;  // @audit CAMBIAR A FINAL
         uint256 usdLimitPhase0_ = 2 * 1e18; // @audit CAMBIAR A FINAL
         uint256 usdLimitPhase1_ = 2 * 1e18; // @audit CAMBIAR A FINAL
 
-        phases_[0] = [200_000_000 * 10**18, 5000, 1737897226]; // @audit CAMBIAR A FINAL
-        phases_[1] = [700_000_000 * 10**18, 15000, 1737997226]; // @audit CAMBIAR A FINAL
-        phases_[2] = [700_000_000 * 10**18, 30000, 1738897226]; // @audit CAMBIAR A FINAL
+        phases_[0] = [20000  * 10**18, 5000, 1737897226]; // @audit CAMBIAR A FINAL
+        phases_[1] = [20000  * 10**18, 15000, 1737997226]; // @audit CAMBIAR A FINAL
+        phases_[2] = [20000  * 10**18, 30000, 1738897226]; // @audit CAMBIAR A FINAL
         
         PresaleL2 presale = new PresaleL2(usdtAddress_, usdcAddress_, aggregatorContract_, paymentWallet_, phases_, maxTotalSellingAmount_, usdLimitPhase0_, usdLimitPhase1_);
 
