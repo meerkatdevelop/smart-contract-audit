14b1285e7004d7e948d056e7c2626f0654d3e16e
CryptoGhost277 <cryptoghost277@gmail.com>
2025-03-16 19:54:18 +0100

    set redeable supply

--- src/MeerkatToken.sol
+++ src/MeerkatToken.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.24;
 import '../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol';
 
 contract MeerkatToken is ERC20 {
-  uint256 _totalSupply = 600000000 * 1e18;
+  uint256 _totalSupply = 600_000_000 * 1e18;
 
   constructor(address distributorAddress_) ERC20('Meerkat', 'MRKT') {
     _mint(distributorAddress_, _totalSupply);
